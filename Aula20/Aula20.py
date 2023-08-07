cb8fcede64570897a9750c3e5061f97486a749d7
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-08-07 15:26:49 -0300

    Exercicio-3

---
+++ Aula20/Aula20.py
@@ -0,0 +1,10 @@
+# Exercicio
+primeiro_Valor=input("Digite um valor: ")
+segundo_Valor=input("Digite outro valor: ")
+if primeiro_Valor > segundo_Valor:
+    print(f' o primeiro Numero: {primeiro_Valor} e maior que segundo: {segundo_Valor}')
+elif primeiro_Valor < segundo_Valor:
+    print(f" Numero dois: {segundo_Valor} e maior que primeiro: {primeiro_Valor}")
+
+
+# aula 40
