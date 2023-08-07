a082a8aa8976f026b91f88de21c85e53d1056854
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-08-07 13:33:32 -0300

    Exercicio-1

---
+++ Exercicio8/Aula8.py
@@ -0,0 +1,13 @@
+# Exercicio 
+nome="Felipe "
+sobrenome="De Oliveira Santos"
+ano_nascimento=2000
+idade=2023-2000
+Comparando=idade >=18
+altura_metros=float(18)
+print("Nome:",nome)
+print("Sobrenome:",sobrenome)
+print("idade:",idade)
+print("ano de Nascimento:",ano_nascimento)
+print("é maior de idade?:",Comparando)
+print("altura em metros:",altura_metros)
