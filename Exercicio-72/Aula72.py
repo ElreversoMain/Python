6ec11497d40e910a2076c9669ef2c05abbed39d9
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-10-23 13:40:01 -0300

    Terminado

--- Exercicio-72/Aula72.py
+++ Exercicio-72/Aula72.py
@@ -1,8 +1,16 @@
 
 def Texte():
-    x=2
+    x=16
     if x % 2 == 0:
-        print(f"numero {x} é impar")
+        print(f"numero {x} é par")
     else:
-        print(f"numero {x} e par")
+        print(f"numero {x} e impar")
 Texte()
+
+def soma (*args):
+    total=1
+    for numero in args:
+        total *=numero
+    return total
+multiplicacao=1*2*3*4*5*10
+print(multiplicacao)
